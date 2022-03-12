9472d0e92607893f51f4fb10e264f62c54762f77
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-12 21:43:16 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,21 @@
+const one = {
+    iLovebackend : uiisdogshit
+}
+
+const two = {
+    everyuidevelopment : isdogshit
+}
+
+
+const three = {
+    yes : everyuiisgarbage
+}
+
+const four = {
+    webui : isdogshit,
+    mobileui : isdogshit,
+    systemui : isdogshit,
+    uidevelopers : aredogshitdevelopers
+}
+
+console.log(four.mobileui)
